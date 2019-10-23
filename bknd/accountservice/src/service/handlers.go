8f3a48c92d037c89d601c63d58d52b2d58d9a0a5
illia.ushakov <illia_ushakov@openjawtech.com>
2019-10-23 11:12:15 +0200

    first commit

---
+++ bknd/accountservice/src/service/handlers.go
@@ -0,0 +1,120 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strconv"
+
+	"dbclient"
+	"model"
+
+	"github.com/gorilla/mux"
+)
+
+var DBClient dbclient.IBoltClient
+
+var isHealthy = true
+
+var client = &http.Client{}
+
+func init() {
+	var transport http.RoundTripper = &http.Transport{
+		DisableKeepAlives: true,
+	}
+	client.Transport = transport
+}
+
+func GetAccount(w http.ResponseWriter, r *http.Request) {
+
+	// Read the 'accountId' path parameter from the mux map
+	var accountId = mux.Vars(r)["accountId"]
+
+	// Read the account struct BoltDB
+	account, err := DBClient.QueryAccount(accountId)
+	account.ServedBy = getIP()
+
+	// If err, return a 404
+	if err != nil {
+		fmt.Println("Some error occured serving " + accountId + ": " + err.Error())
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// NEW call the quotes-service
+	quote, err := getQuote()
+	if err == nil {
+		account.Quote = quote
+	}
+
+	// If found, marshal into JSON, write headers and content
+	data, _ := json.Marshal(account)
+	writeJsonResponse(w, http.StatusOK, data)
+}
+
+func getQuote() (model.Quote, error) {
+	req, _ := http.NewRequest("GET", "http://quotes-service:8080/api/quote?strength=4", nil)
+	resp, err := client.Do(req)
+
+	if err == nil && resp.StatusCode == 200 {
+		quote := model.Quote{}
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(bytes, &quote)
+		return quote, nil
+	} else {
+		return model.Quote{}, fmt.Errorf("Some error")
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	// Since we're here, we already know that HTTP service is up. Let's just check the state of the boltdb connection
+	dbUp := DBClient.Check()
+	if dbUp && isHealthy {
+		data, _ := json.Marshal(healthCheckResponse{Status: "UP"})
+		writeJsonResponse(w, http.StatusOK, data)
+	} else {
+		data, _ := json.Marshal(healthCheckResponse{Status: "Database unaccessible"})
+		writeJsonResponse(w, http.StatusServiceUnavailable, data)
+	}
+}
+
+func SetHealthyState(w http.ResponseWriter, r *http.Request) {
+	// Read the 'accountId' path parameter from the mux map
+	var state, err = strconv.ParseBool(mux.Vars(r)["state"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	isHealthy = state
+	w.WriteHeader(http.StatusOK)
+}
+
+func writeJsonResponse(w http.ResponseWriter, status int, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(status)
+	w.Write(data)
+}
+
+func getIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "error"
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	panic("Unable to determine local IP address (non loopback). Exiting.")
+}
+
+type healthCheckResponse struct {
+	Status string `json:"status"`
+}
