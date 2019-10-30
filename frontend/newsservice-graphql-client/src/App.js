630e487e3d91340299fe8fef177e7c01b1f8bddd
illia.ushakov <illia_ushakov@openjawtech.com>
2019-10-30 18:11:14 +0100

    react part get all news via graphql

--- frontend/newsservice-graphql-client/src/App.js
+++ frontend/newsservice-graphql-client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, BrowserRouter } from 'react-router-dom';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
-import News from './containers/components/News'
+// import News from './containers/components/News'
+import Layout from './containers/layouts/Layout';
+import Home from './containers/layouts/Home';
 import './App.css';
 
 const NEWSSERVICE_BASE_URL = `${process.env.REACT_APP_API_PROXY}/graphql`
@@ -15,9 +18,17 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <div>
+    <BrowserRouter>
+        <Layout>
+            <Route exact path="/" component={Home } />
+            {/* <Route path='/news/:id' component={NewsArticle}/>  
+            <Route path='/submit' component={NewsSubmit}/>   */}
+        </Layout> 
+    </BrowserRouter>
+
+    {/* <div>
       <News/>
-    </div>
+    </div> */}
   </ApolloProvider>
 );
 
---
+++ frontend/newsservice-graphql-client/src/containers/layouts/Home.js
@@ -0,0 +1,15 @@
+import React
+from 'react';
+import News from '../components/News';
+
+const Home = () => (
+        <div>
+            <div>Welcome to MyNews.com</div> 
+            <div> 
+                <News />
+            </div> 
+        </div>
+        
+);
+
+export default Home;
