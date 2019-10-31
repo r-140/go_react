4541dbb0adf794292dbbe2b248cfe382b8240f0f
illia.ushakov <illia_ushakov@openjawtech.com>
2019-10-31 13:55:12 +0100

    docker-compose

--- frontend/newsservice-graphql-client/src/containers/presentation/NewsItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const NewsItem = (props) => (
-    <div key={props.item.id}>
-        <div><Link to={`/news/${props.item.id}`}><b>{props.item.title}</b></Link></div> 
-    </div>
-);
-export default NewsItem;
